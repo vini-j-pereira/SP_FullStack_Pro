c88b78eee38af05e809dc598c2dd6b1a693f60df
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-05-26 22:53:52 -0300

    commit_ function manipulando html

--- instagram Projeto/JavaScript/Inicio/aula01.js
+++ instagram Projeto/JavaScript/Inicio/aula01.js
@@ -14,7 +14,19 @@ function entrar(){
         area.innerHTML = 'Algo deu errado, clique no botão para continuar...';
 
     }else{
-        area.innerHTML = ` Bem vindo meu caro amigo ${nome}!`;
+        area.innerHTML = ` Bem vindo meu caro amigo ${nome}! \n`;
+
+        let botaoSair = document.createElement("button");
+        botaoSair.innerHTML = " Sair da conta";
+
+        botaoSair.onclick = sair;
+
+        area.appendChild(botaoSair);
     }
     
+}
+
+function sair(){
+    alert("Volte sempre!");
+    area.innerHTML = `Você saiu.`;
 }
