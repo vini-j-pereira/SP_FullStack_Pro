eefec42b2014a73af45a7980db36443d042fcf0b
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-05-28 14:51:12 -0300

    commit_ Loops

---
+++ JavaScript/Inicio/aula03loops.js
@@ -0,0 +1,27 @@
+// while = enquanto
+
+//  var x = "";
+
+//  x = Number(prompt("Digite o valor de X"))
+
+//  while(x <= 10){
+//     document.write("<br> O valor de X é: " + x);
+
+//     //Aumentar o valor do x
+//      x++;
+//  };
+
+
+//-----------------------------------------
+
+
+// For = Para
+
+// var a = Number(prompt("Digite o valor de a:"));
+
+// var valor = Number(prompt("Digite o valor de Valor:"));
+
+// for(a = 0; a <= valor; a++){
+//     document.write("<br> Valor do A é: " + a);
+
+// };
