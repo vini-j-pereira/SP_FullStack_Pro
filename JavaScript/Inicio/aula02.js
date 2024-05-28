de5b6f46ab0cdf4b8329697b90c75defde38e69e
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-05-27 22:53:42 -0300

    commit_ media aluno

---
+++ JavaScript/Inicio/aula02.js
@@ -0,0 +1,10 @@
+function mediaAluno(nota1, nota2){
+    let media = (nota1 + nota2) / 2;
+
+    if( media >= 7){
+        console.log('Aluno foi aprovado');
+    }else if(media < 7){
+        console.log('Aluno reprovado');
+    }
+
+}
