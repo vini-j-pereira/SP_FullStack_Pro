1efb9d47f7f0e4d5dc927580d15f729c2ff85785
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-05-29 22:36:30 -0300

    commit_ temporizadores

---
+++ JavaScript/Inicio/JS avançadp/temporizadores.js
@@ -0,0 +1,31 @@
+// Temporizadores:
+
+//--- setInterval --- (Aplicações)==>  relogios, contadores...  
+
+// function acao(){
+//     document.write("Executando... <br>");
+// }
+// setInterval(acao, 1000);
+
+
+//Função Anonima --------
+// setInterval(() => {
+//     document.write("Executando....... <br>");
+// }, 5000);
+
+// para paudar o setInterval é necessario atrela-lo a uma variavel: ---------
+// let timer = setInterval(() => {
+//     document.write("Executando....... <br>");
+// }, 5000);
+
+// clearInterval(timer); // ------ comando para chamar a variavel que pausa a repetição
+
+
+
+
+//--- setTimeout ---
+
+// function acao(){
+//     document.write("Executando o TimeOut!");
+// }
+// setTimeout(acao, 1000);
