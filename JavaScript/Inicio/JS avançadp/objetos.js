5a72f604c057d4b5299ddc10c8129ffae6707d73
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-05-30 22:10:07 -0300

    commit_ objetos e listas

---
+++ JavaScript/Inicio/JS avançadp/objetos.js
@@ -0,0 +1,30 @@
+// (Objetos)---
+
+// ex. de um Objeto:
+let pessoa = {
+    nome: "Vinicius",
+    idade: 37,
+    altura: 1.75,
+    cargo: "Programador FullStack"
+};
+
+
+//ex. de uma lista de objetos:
+
+let usuarios = [
+    {nome: "Vinicius", idade: 37, altura: 1.75, cargo: "Programador FullStack"},
+    {nome: "Théo", idade: 36, altura: 1.68, cargo: "Engenheiro"},
+    {nome: "Matheus", idade: 34, altura: 1.78, cargo: "Mecanico"}
+    
+];
+
+//Buscando informações da lista:
+
+usuarios[1]; `vais me retornar todo o objeto 1, {nome: "Théo", idade: 36, altura: 1.68, cargo: "Engenheiro"}`;
+
+// Agora dentro desse objeto vou buscar uma informação desejada:
+
+usuarios[1].idade; `agora é vai me retornar somento a idade = (36)`;
+
+
+
