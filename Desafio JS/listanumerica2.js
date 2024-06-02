ee09e334d670846933aa01ebec3ee53c66117f42
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-06-01 21:14:15 -0300

    commit_ lista2 invert

---
+++ Desafio JS/listanumerica2.js
@@ -0,0 +1,11 @@
+let numeros = [5, 2, 12, 57, 13, 25, 11, 15, 10]
+
+numeros.splice(0, 1)
+
+// numeros.sort(function(a, b){
+//     return a - b;
+// })
+
+let numerosInvert = numeros.reverse();
+
+document.write(numerosInvert);
