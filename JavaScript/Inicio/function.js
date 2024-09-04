dd86a517677eee01fd17d407a65744c493bc4f0b
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-09-04 15:49:38 -0300

    aula sp function and array

---
+++ JavaScript/Inicio/function.js
@@ -0,0 +1,49 @@
+//Funções------------------------------------
+
+// var area = document.getElementById('area');
+
+// function entrar(){
+//     var nome = prompt("Digite seu nome!")
+
+//     if(nome === '' || nome === null) {
+//         alert("Ops! algo deu errado!")
+
+//         area.innerHTML = "Clique no botão para acessar...";
+//     } else {
+//         area.innerHTML = "Bem Vindo " + nome + "! ";
+
+//         let botaoSair = document.createElement("button");
+//         botaoSair.innerHTML = "Sair da conta";
+//         botaoSair.onclick = sair;
+
+//         area.appendChild(botaoSair);
+//     }
+
+// }
+
+// function sair() {
+//     alert("Até mais!");
+//     area.innerHTML = "Você saiu da conta!";
+// }
+
+// function mediaAluno(nota1, nota2) {
+//     var media = ( nota1 + nota2) / 2;
+
+//     if(media >= 7){
+//         console.log("Aluno aprovado coma a media: " + media);
+
+//     } else if(media < 7){
+//         console.log("Aluno reprovado com media: " + media);
+//     }
+// }
+
+
+// function aluno(nome, curso) {
+//     var msg = `Seja bem vindo ${nome} ao curso de ${curso}`;
+
+//     console.log(msg);
+// }
+
+//Array----------------------------------------
+
+let lista = ['Vini', 'Jose', 'Theo', 'Matheus', 13];
