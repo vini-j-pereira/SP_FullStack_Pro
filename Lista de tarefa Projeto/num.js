6efe02c462ca7fef872d03983dec2b1a1d9d6d98
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-09-13 00:14:39 -0300

    save num

--- Lista de tarefa Projeto/num.js
+++ Lista de tarefa Projeto/num.js
@@ -1,14 +1,52 @@
-inputNum = document.querySelectorAll("#app input");
+inputNum = document.querySelector("#app input");
 btnNum = document.querySelector("#app button");
 ulNum = document.querySelector("#app ul");
 
 let listaNum = [];
 
+function renderNum() {
+    ulNum.innerHTML = "";
+
+    listaNum.map((numDaLista) => {
+        let liElement = document.createElement("li");
+        let numDescri = document.createTextNode(numDaLista);
+
+        let linkNum = document.createElement("a");
+        linkNum.setAttribute("href", "#");
+        let numText = document.createTextNode("Deletar");
+        linkNum.appendChild(numText);
+
+        let position = listaNum.indexOf(numDaLista);
+
+        linkNum.setAttribute('onclick', `deletarNum(${position})`);
+
+        liElement.appendChild(numDescri);
+        liElement.appendChild(linkNum);
+        ulNum.appendChild(liElement)
+    })
+}
+
 
 function calcularNum() {
-    let numero = Number(inputNum.value)
-    console.log(numero)
-    
+
+    if(inputNum.value == ''){
+        alert("Digite um numero!")
+        return false;
+
+    } else {
+        let numero = Number(inputNum.value);
+        listaNum.push(numero);
+
+        inputNum.value = "";
+
+        renderNum();
+    }   
 }
 
 btnNum.onclick = calcularNum;
+
+function deletarNum(position) {
+    listaNum.splice(position, 1);
+
+    renderNum();
+}
