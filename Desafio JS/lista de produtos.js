ac628ae033dbb3b4097dd4659ce4052eb71d6dca
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-09-14 21:59:54 -0300

    ex array sort and splice

--- Desafio JS/lista de produtos.js
+++ Desafio JS/lista de produtos.js
@@ -1,20 +1,29 @@
-let produtos = ["Computador", "Telefone", "Mouse", "Teclado"];
+// let produtos = ["Computador", "Telefone", "Mouse", "Teclado"];
 
-produtos.splice(2, 1);
-produtos.splice(1, 1);
+// produtos.splice(2, 1);
+// produtos.splice(1, 1);
 
-let resultado = produtos.filter((item) => {
+// let resultado = produtos.filter((item) => {
 
-    if(item === "Computador"){
-        console.log(`Esse item existe na lista ${item}`)
-    }else{
-        console.log(`Esse item não existe na lista.`)
-    }
+//     if(item === "Computador"){
+//         console.log(`Esse item existe na lista ${item}`)
+//     }else{
+//         console.log(`Esse item não existe na lista.`)
+//     }
 
-})
+// })
 
 
 
-document.write(produtos);
+// document.write(produtos);
+let lista = [1, 3, 5, 10, 0, 19, 51, 13, 5];
+lista.splice(0, 1);
+lista.splice(1, 1)
+
+lista.sort((a, b) => b - a); 
+
+console.log(lista);
+
+
 
 
