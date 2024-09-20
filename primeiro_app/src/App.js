0388a15737f5c0bfbadf4754fa1d87cbc6b2a625
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-09-20 01:17:51 -0300

    useState

--- primeiro_app/src/App.js
+++ primeiro_app/src/App.js
@@ -1,62 +1,101 @@
+// import { useState } from "react";
+
+
+// function App() {
+
+//   const[nome, setNome] = useState('');
+//   const[email, setEmail] = useState('');
+//   const[idade, setIdade] = useState('');
+
+//   const[user, setUser] = useState({});
+
+//   function handleRegister(e){
+//     e.preventDefault();
+
+//     alert('Usuario registrado com sucesso!')
+
+//     setUser({
+//       nome: nome,
+//       idade: idade,
+//       email: email,
+//     })
+//   }
+
+//   return (
+//     <div>
+//      <form onSubmit={handleRegister}>
+//       <label>Nome:</label><br></br>
+//       <input 
+//       placeholder="Digite seu nome:"
+//       value={nome}
+//       onChange={(e) => setNome(e.target.value)}
+//       ></input><br></br>
+
+//       <label>E-mail:</label><br></br>
+//       <input 
+//       placeholder="Digite seu email:"
+//       value={email}
+//       onChange={(e) => setEmail(e.target.value)}
+//       ></input><br></br>
+
+//       <label>Idade:</label><br></br>
+//       <input 
+//       placeholder="Digite seu idade:"
+//       value={idade}
+//       onChange={(e) => setIdade(e.target.value)}
+//       ></input><br></br>
+
+//       <button type="submit">Registrar</button>
+//      </form>
+
+//      <br/><br/>
+
+//       <div>
+//         <span>Bem vindo: {user.nome} </span><br></br>
+//         <span>Idade: {user.idade} </span><br></br>
+//         <span>E-mail: {user.email} </span><br></br>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { useState } from "react";
 
-
-
-
-function App() {
-
-  const[nome, setNome] = useState('');
-  const[email, setEmail] = useState('');
-  const[idade, setIdade] = useState('');
-
-  const[user, setUser] = useState({});
+function App(){
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState(['Pagar a conta de luz']);
 
   function handleRegister(e){
     e.preventDefault();
 
-    alert('Usuario registrado com sucesso!')
-
-    setUser({
-      nome: nome,
-      idade: idade,
-      email: email,
-    })
+    setTarefas([...tarefas, input]);
+    setInput('');
   }
 
-  return (
+  return(
     <div>
-     <form onSubmit={handleRegister}>
-      <label>Nome:</label><br></br>
-      <input 
-      placeholder="Digite seu nome:"
-      value={nome}
-      onChange={(e) => setNome(e.target.value)}
-      ></input><br></br>
 
-      <label>E-mail:</label><br></br>
-      <input 
-      placeholder="Digite seu email:"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      ></input><br></br>
+      <h1>Lista de Tarefas</h1>
 
-      <label>Idade:</label><br></br>
-      <input 
-      placeholder="Digite seu idade:"
-      value={idade}
-      onChange={(e) => setIdade(e.target.value)}
-      ></input><br></br>
+      <form onSubmit={handleRegister}>
+        <input 
+        placeholder="Digite uma tarefa:"
+        value={input}
+        onChange={ (e) => setInput(e.target.value)}
+        ></input><br/>
+        <button type="submit">Salvar</button>
+      </form>
 
-      <button type="submit">Registrar</button>
-     </form>
+      <br/><br/>
 
-     <br/><br/>
-
-      <div>
-        <span>Bem vindo: {user.nome} </span><br></br>
-        <span>Idade: {user.idade} </span><br></br>
-        <span>E-mail: {user.email} </span><br></br>
-      </div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      
     </div>
   );
 }
